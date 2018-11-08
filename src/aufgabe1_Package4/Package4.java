8c2a8f407252c4c0f73d26012c814e820907ee78
kilianbauer <1710653657@stud.fh-kufstein.ac.at>
2018-11-08 14:52:54 +0100

    fünfter commit

---
+++ src/aufgabe1_Package4/Package4.java
@@ -0,0 +1,15 @@
+package aufgabe1_Package4;
+
+public class Package4 {
+
+    private String autofill;
+
+    public static void main(String args[]) {
+        /*Verwenden der Autovervollständigung*/
+        /*STRG+Alt+L formatiert den Code*/
+
+        final String autofill = "Autovervollständigung";
+
+    }
+
+}
